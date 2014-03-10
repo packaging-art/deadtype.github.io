ee6865ce331372170b0a1d6bd95820183233a66e
deadtype <yagnyuk@me.com>
2014-03-09 22:58:36 -0700

    Remove

--- _site/js/system/picturefill/picturefill.js
+++
@@ -1,5 +0,0 @@
-(function(w){"use strict";w.picturefill=function(){var ps=w.document.getElementsByTagName("span");for(var i=0,il=ps.length;i<il;i++){if(ps[i].getAttribute("data-picture")!==null){var sources=ps[i].getElementsByTagName("span"),matches=[];for(var j=0,jl=sources.length;j<jl;j++){var media=sources[j].getAttribute("data-media");if(!media||(w.matchMedia&&w.matchMedia(media).matches)){matches.push(sources[j]);}}
-var picImg=ps[i].getElementsByTagName("img")[0];if(matches.length){var matchedEl=matches.pop();if(!picImg||picImg.parentNode.nodeName==="NOSCRIPT"){picImg=w.document.createElement("img");picImg.alt=ps[i].getAttribute("data-alt");}
-picImg.src=matchedEl.getAttribute("data-src");matchedEl.appendChild(picImg);}
-else if(picImg){picImg.parentNode.removeChild(picImg);}}}};if(w.addEventListener){w.addEventListener("resize",w.picturefill,false);w.addEventListener("DOMContentLoaded",function(){w.picturefill();w.removeEventListener("load",w.picturefill,false);},false);w.addEventListener("load",w.picturefill,false);}
-else if(w.attachEvent){w.attachEvent("onload",w.picturefill);}}(this));
